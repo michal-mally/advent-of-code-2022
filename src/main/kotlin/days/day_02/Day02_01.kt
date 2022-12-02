b927b1d43697c0cec9165f1b9caafaaf9968680f
Michał Mally <michal@mally.pl>
2022-12-02 06:55:50 +0100

    Day 2: Improved version of the part 1.

--- src/main/kotlin/days/day_02/Day02_01.kt
+++ src/main/kotlin/days/day_02/Day02_01.kt
@@ -8,20 +8,13 @@ fun main() {
     lines(2)
         .map { it.split(" ") }
         .map { it[0] to it[1] }
-        .map { it.mapBoth { it[0].code } }
+        .map { it.mapBoth { value -> value[0].code } }
         .map { (it.first - 'A'.code + 1) to (it.second - 'X'.code + 1) }
-        .map { it.mapLeft { l, r -> result(l, r) } }
+        .map { it.mapLeft { l, r -> r - l + 4 } }
+        .map { it.mapLeft { l, _ -> l % 3 } }
+        .map { it.mapLeft { l, _ -> l * 3 } }
         .map { it.first + it.second }
         .sum()
         .let(::println)
 }
 
-fun result(left: Int, right: Int): Int {
-    val right = if (right < left) right + 3 else right
-    return when(right - left) {
-        0 -> 3
-        1 -> 6
-        2 -> 0
-        else -> throw IllegalArgumentException()
-    }
-}
