155daf325d3398f7502abc422cc21319542411a2
Michał Mally <michal@mally.pl>
2022-12-09 06:42:40 +0100

    Day 8: Improvements.

--- src/main/kotlin/days/day_08/Day08_1.kt
+++ src/main/kotlin/days/day_08/Day08_1.kt
@@ -12,31 +12,33 @@ class Day08_1 : Solver<Sequence<String>, Int> {
             .let(::TwoDimArray)
 
         val visibleTrees = mutableSetOf<Pair<Int, Int>>()
-        listOf(
-            forest
-                .rowIndices
-                .map { row -> forest.columnIndices.map { row to it } },
-            forest
-                .rowIndices
-                .map { row -> forest.columnIndices.reversed().map { row to it } },
-            forest
-                .columnIndices
-                .map { col -> forest.rowIndices.map { it to col } },
-            forest
-                .columnIndices
-                .map { col -> forest.rowIndices.reversed().map { it to col } },
-        )
-            .flatten()
-            .map {
-                var tallestTree = -1
-                for (position in it) {
-                    val tree = forest[position]
-                    if (tree > tallestTree) {
-                        tallestTree = tree
-                        visibleTrees += position
-                    }
-                }
-            }
+        for (direction in forest.allDirections()) {
+            forest.visibleTreesInDirection(direction, visibleTrees)
+        }
+
         return visibleTrees.size
     }
+
+    private fun TwoDimArray<Int>.visibleTreesInDirection(
+        direction: List<Pair<Int, Int>>,
+        visibleTrees: MutableSet<Pair<Int, Int>>,
+    ) {
+        var tallestTree = -1
+        for (position in direction) {
+            val tree = this@TwoDimArray[position]
+            if (tree > tallestTree) {
+                tallestTree = tree
+                visibleTrees += position
+            }
+        }
+    }
+
+    private fun TwoDimArray<Int>.allDirections() =
+        sequenceOf(
+            rowIndices.map { row -> columnIndices.map { row to it } },
+            rowIndices.map { row -> columnIndices.reversed().map { row to it } },
+            columnIndices.map { col -> rowIndices.map { it to col } },
+            columnIndices.map { col -> rowIndices.reversed().map { it to col } },
+        )
+            .flatten()
 }
--- src/main/kotlin/days/day_08/Day08_2.kt
+++ src/main/kotlin/days/day_08/Day08_2.kt
@@ -4,38 +4,48 @@ import util.Solver
 import util.array.TwoDimArray
 
 class Day08_2 : Solver<Sequence<String>, Int> {
-    override fun solve(input: Sequence<String>) =
+    override fun solve(input: Sequence<String>): Int {
+        val forest = forest(input)
+        return forest
+            .allPositions()
+            .filter { it.row in (1..<forest.rowCount - 1) && it.column in (1..<forest.columnCount - 1) }
+            .maxOf { forest.scenicViewOf(it) }
+    }
+
+    private fun forest(input: Sequence<String>) =
         input
             .map { it.toList() }
             .map { it.map { c -> c.toString().toInt() } }
             .toList()
             .let(::TwoDimArray)
-            .let { forest ->
-                with(forest) {
-                    allPositions()
-                        .filter { it.row in (1..<forest.rowCount - 1) && it.column in (1..<forest.columnCount - 1) }
-                        .maxOf { scenicViewOf(it) }
-                }
-            }
 
-    context(TwoDimArray<Int>) private fun scenicViewOf(position: Pair<Int, Int>) =
-        listOf(
-            columnIndices.filter { it < position.column }.reversed().map { position.row to it },
-            columnIndices.filter { it > position.column }.map { position.row to it },
-            rowIndices.filter { it < position.row }.reversed().map { it to position.column },
-            rowIndices.filter { it > position.row }.map { it to position.column },
-        )
-            .map {
-                var visibleTrees = 0
-                for (p in it) {
-                    visibleTrees++
-                    if (this@TwoDimArray[p] >= this@TwoDimArray[position]) {
-                        break
-                    }
-                }
-                visibleTrees
-            }
+    private fun TwoDimArray<Int>.scenicViewOf(position: Pair<Int, Int>): Int {
+        val treeHeight = this@TwoDimArray[position]
+        return allDirections(position)
+            .map { direction -> direction.map { this@TwoDimArray[it] } }
+            .map { visibleTreesInDirection(treeHeight, it) }
             .reduce(Int::times)
+    }
+
+    private fun visibleTreesInDirection(treeHeight: Int, otherTreeHeights: Sequence<Int>): Int {
+        var visibleTrees = 0
+        for (otherTreeHeight in otherTreeHeights) {
+            visibleTrees++
+            if (otherTreeHeight >= treeHeight) {
+                break
+            }
+        }
+
+        return visibleTrees
+    }
+
+    private fun TwoDimArray<Int>.allDirections(position: Pair<Int, Int>) =
+        sequenceOf(
+            (position.column - 1 downTo 0).asSequence().map { position.row to it },
+            (position.column + 1..<columnCount).asSequence().map { position.row to it },
+            (position.row - 1 downTo 0).asSequence().map { it to position.column },
+            (position.row + 1..<rowCount).asSequence().map { it to position.column },
+        )
 
     private val <L, R> Pair<L, R>.row
         get() = first
