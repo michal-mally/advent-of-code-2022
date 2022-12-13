30d980706d5c291c7d4d4a0220a0b357a5a95085
Michał Mally <michal@mally.pl>
2022-12-13 09:18:22 +0100

    Day 13: Part I.

---
+++ src/test/kotlin/days/day_13/Day13Spec.kt
@@ -0,0 +1,13 @@
+package days.day_13
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import util.file.lines
+
+class Day13Spec : FreeSpec({
+
+    "Part 1" {
+        Day13_1().solve(lines(13)) shouldBe 6568
+    }
+
+})
