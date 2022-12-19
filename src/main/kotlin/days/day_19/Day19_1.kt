a8865b9bdab907cc9085ad12bbc5b7f5c1f5d782
Michał Mally <michal@mally.pl>
2022-12-19 18:49:25 +0100

    Day 19: General game mechanism. Not returning optimal result yet.

--- src/main/kotlin/days/day_19/Day19_1.kt
+++ src/main/kotlin/days/day_19/Day19_1.kt
@@ -2,22 +2,61 @@ package days.day_19
 
 import util.Solver
 
-private val regex =
-    """Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.""".trimIndent()
-        .toRegex()
-
 class Day19_1 : Solver<Sequence<String>, Int> {
-    override fun solve(input: Sequence<String>): Int {
-        val blueprints = input
-            .map { parseBlueprint(it) }
-            .toList()
+    override fun solve(input: Sequence<String>) =
+        input
+            .map(::parseProductionCosts)
+            .mapIndexed { index, blueprint -> (index + 1) * evaluate(blueprint) }
+            .sum()
 
-        blueprints.forEach(::println)
+    private fun evaluate(productionCosts: Map<String, Map<String, Int>>): Int {
+        val robots = mutableMapOf("ore" to 1).withDefault { 0 }
+        val resources = mutableMapOf<String, Int>().withDefault { 0 }
 
-        TODO("Not yet implemented")
+        repeat(24) { minute ->
+            println("== Minute ${minute + 1} ==")
+            val nextProduction = decideNextProduction(productionCosts, robots, resources)
+            if (nextProduction != null) {
+                println(
+                    "Spend ${
+                        productionCosts.getValue(nextProduction).map { (resource, amount) -> "$amount $resource" }
+                            .joinToString(" and ")
+                    } to start building ${if (nextProduction.startsWith("o")) "an" else "a"} $nextProduction-collecting robot."
+                )
+                productionCosts
+                    .getValue(nextProduction)
+                    .forEach { (resource, cost) ->
+                        resources[resource] = resources.getValue(resource) - cost
+                    }
+            }
+            robots.forEach { (resource, amount) ->
+                resources[resource] = resources.getValue(resource) + amount
+                println("$amount $resource-collecting ${if (amount == 1) "robot collects" else "robots collect"} $amount $resource; you now have ${resources[resource]} $resource.")
+            }
+            if (nextProduction != null) {
+                robots[nextProduction] = robots.getValue(nextProduction) + 1
+                println("The new $nextProduction-collecting robot is ready; you now have ${robots[nextProduction]} of them.")
+            }
+            println()
+        }
+
+        return resources.getValue("geode")
     }
 
-    private fun parseBlueprint(blueprint: String) =
+    private fun decideNextProduction(
+        productionCosts: Map<String, Map<String, Int>>,
+        robots: Map<String, Int>,
+        resources: Map<String, Int>
+    ): String? {
+        fun canBeBuilt(robotType: String) =
+            productionCosts
+                .getValue(robotType)
+                .all { (resource, amount) -> resources.getValue(resource) >= amount }
+
+        return sequenceOf("geode", "obsidian", "clay", "ore").firstOrNull { canBeBuilt(it) }
+    }
+
+    private fun parseProductionCosts(blueprint: String) =
         blueprint
             .substringAfter(": ")
             .splitToSequence("""\.\s?""".toRegex())
