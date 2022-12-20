6a6d7d6e3b2f8b3065943307c02fcd22fe74510f
Michał Mally <michal@mally.pl>
2022-12-20 06:52:53 +0100

    Day 20: Part II.

--- src/test/kotlin/days/day_20/Day20Spec.kt
+++ src/test/kotlin/days/day_20/Day20Spec.kt
@@ -7,7 +7,11 @@ import util.file.lines
 class Day20Spec : FreeSpec({
 
     "Part 1" {
-        Day20_1().solve(lines(20)) shouldBe 1192
+        Day20_1().solve(lines(20)) shouldBe 11037
+    }
+
+    "Part 2" {
+        Day20_2().solve(lines(20)) shouldBe 3033720253914L
     }
 
 })
