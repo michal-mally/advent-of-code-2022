a63097b1f0418d2c54a9e33d0ab0a5ddce9dc194
Michał Mally <michal@mally.pl>
2022-12-29 00:51:51 +0100

    Day 22: Refactoring.

--- src/main/kotlin/days/day_22/Day22_2.kt
+++ src/main/kotlin/days/day_22/Day22_2.kt
@@ -11,23 +11,20 @@ private const val SIDE_SIZE = 50
 
 class Day22_2 : Solver<Sequence<String>, Int> {
 
-    override fun solve(input: Sequence<String>): Int {
-        val (map, instructionsRaw) = input
+    override fun solve(input: Sequence<String>) =
+        input
             .splitBy { it.isBlank() }
             .toList()
+            .let { (map, instructionsRaw) -> sides(map, SIDE_SIZE) to instructions(instructionsRaw) }
+            .let { (sides, instructions) -> Game(sides).simulate(instructions) }
+            .score()
 
-        return Game(sides(map, SIDE_SIZE))
-            .simulate(instructions(instructionsRaw))
-            .let(::score)
-    }
-
-    private fun score(locationAndDirection: LocationAndDirection) =
-        locationAndDirection
-            .universalLocation
+    private fun LocationAndDirection.score() =
+        universalLocation
             .plus(Point(1 to 1))
             .times(Point(4 to 1_000))
             .toList()
             .sum()
-            .plus(locationAndDirection.direction.ordinal)
+            .plus(direction.ordinal)
 
 }
