f096158b74729349e91c913966949091ad8901b7
Michał Mally <michal@mally.pl>
2022-12-29 09:07:00 +0100

    Day 22: Refactoring.

--- src/main/kotlin/util/point/DoublePoints.kt
+++ src/main/kotlin/util/point/DoublePoints.kt
@@ -5,16 +5,17 @@ import kotlin.math.cos
 import kotlin.math.roundToInt
 import kotlin.math.sin
 
-fun Point<Double>.rotate(degreesClockwise: Double, around: Point<Double> = Point(0.0 to 0.0)): Point<Double> =
-    minus(around)
-        .let { (x, y) ->
-            val radians = toRadians(-degreesClockwise)
-            val cos = cos(radians)
-            val sin = sin(radians)
+fun Point<Double>.transformTranslated(translation: Point<Double>, f: Point<Double>.() -> Point<Double>): Point<Double> =
+    f(this - translation) + translation
 
-            Point(x * cos - y * sin to x * sin + y * cos)
-        }
-        .plus(around)
+fun Point<Double>.rotate(degreesClockwise: Double, around: Point<Double> = Point(0.0 to 0.0)): Point<Double> =
+    transformTranslated(around) {
+        val radians = toRadians(-degreesClockwise)
+        val cos = cos(radians)
+        val sin = sin(radians)
+
+        Point(x * cos - y * sin to x * sin + y * cos)
+    }
 
 fun Point<Double>.toIntPoint(): Point<Int> =
     Point(x.roundToInt() to y.roundToInt())
