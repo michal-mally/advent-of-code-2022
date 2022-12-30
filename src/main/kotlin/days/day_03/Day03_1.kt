8a96fabb80417f2065e13400084caa315d8f857f
Michał Mally <michal@mally.pl>
2022-12-30 23:17:43 +0100

    Day 3: Reviewed.

--- src/main/kotlin/days/day_03/Day03_1.kt
+++ src/main/kotlin/days/day_03/Day03_1.kt
@@ -1,6 +1,7 @@
 package days.day_03
 
 import util.Solver
+import util.number.isEven
 
 class Day03_1 : Solver<Sequence<String>, Int> {
 
@@ -12,6 +13,7 @@ class Day03_1 : Solver<Sequence<String>, Int> {
     private fun extractCharFromLine(line: String) =
         line
             .toList()
+            .also { require(it.size.isEven()) { "Line split into two equal parts must be possible." } }
             .chunked(line.length / 2)
             .map(List<Char>::toSet)
             .reduce(Set<Char>::intersect)
--- src/main/kotlin/util/number/Numbers.kt
+++ src/main/kotlin/util/number/Numbers.kt
@@ -1,5 +1,8 @@
 package util.number
 
+fun Int.isEven() =
+    this % 2 == 0
+
 fun count(start: Int = 0, block: IntHolder.() -> Unit) =
     IntHolder(start).apply(block).value
 
